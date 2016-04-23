51bf2145320239315bae338020f538f7b35c98a0
mau <mausotog@gmail.com>
2016-04-23 00:22:11 -0400

    fixed count total number of replacements.sh

--- CountTotalNumberOfReplacements.sh
+++ CountTotalNumberOfReplacements.sh
@@ -10,40 +10,46 @@ done
 
 cd $containerFolderName
 
-ls -d */ > GithubProjects.txt
+ls -d *BugFixingCommitVersions > GithubProjects.txt
 
 projectNames=./GithubProjects.txt
 while read projectName 
 do
+
+  echo "current directory:"
+  pwd
   cd $projectName
-  cd BugFixingCommitVersions
+#  cd BugFixingCommitVersions
 
   ls -d Commit[1234567890]*.txt | grep -v / > CommitFiles.txt
 
-  commitFilesNames=./CommitFiles.txt
-  while read commitFileName 
-  do
-    if [ -s $commitFileName ]
+  if [ -e "CommitFiles.txt" ] 
     then
-      echo "Looking at file $commitFileName"
-      for i in `seq 1 484`; # 484 = 22*22 all possible combinations of replacements
-      do
-        commandToGetSpecificLine="sed '$i!d' $commitFileName | cut -f1 -d':'"
-        numberOfReplacementsOfThisLine=$(eval $commandToGetSpecificLine)
-        (( replacementCounter[$i]+=$numberOfReplacementsOfThisLine ))
-        if [ ${replacementCounter[$i]} -ne 0 ]; then
-          echo "Total counter for iterator $i: ${replacementCounter[$i]}"
-        fi
-      done 
-    fi
-  done < $commitFilesNames
-cd .. # get out of BugFixingCommitVersions
+    commitFilesNames=./CommitFiles.txt
+    while read commitFileName 
+    do
+      if [ -s $commitFileName ]
+      then
+        echo "Looking at file $commitFileName"
+        for i in `seq 1 484`; # 484 = 22*22 all possible combinations of replacements
+        do
+          commandToGetSpecificLine="sed '$i!d' $commitFileName | cut -f1 -d':'"
+          numberOfReplacementsOfThisLine=$(eval $commandToGetSpecificLine)
+          (( replacementCounter[$i]+=$numberOfReplacementsOfThisLine ))
+          if [ ${replacementCounter[$i]} -ne 0 ]; then
+            echo "Total counter for iterator $i: ${replacementCounter[$i]}"
+          fi
+        done 
+      fi
+    done < $commitFilesNames
+  fi
+#cd .. # get out of BugFixingCommitVersions
 cd .. # get out of $projectName
 done < $projectNames
 
 #print the replacementCounter array into a file here: Model.txt
 for i in `seq 1 484`; # 484 = 22*22 all possible combinations of replacements
 do
-  echo ${eplacementCounter[$i]} > Model.txt
+  #echo ${replacementCounter[$i] } > Model.txt
 done
 
--- DoWorkOnDownloadedRepos.sh
+++ DoWorkOnDownloadedRepos.sh
@@ -5,56 +5,21 @@
 
 #This program reads a list of git respositories and clones each respository in the #list in the current directory
 
-# Example usage: ./DoWorkOnDownloadedRepos.sh /home/mau/Research/replacements/ReplacementsEmpiricalStudy/qacrashfix/QACrashFix/
+# Example usage: ./DoWorkOnDownloadedRepos.sh
 
 
-QACRASHFIXDIRECTORY="$1"
+QACRASHFIXDIRECTORY=$(pwd)
 
-#The link is hardcoded, and it is the link of the java projects in github sorted by the number of stars
-#pythonArgs="https://github.com/search?utf8=%E2%9C%93&q=language%3AJava+stars%3A%3E1000&type=Repositories&ref=searchresults $NUMBEROFPROJECTSTODOWNLOAD"
-
-#echo "Calling ExtractGitHubRepos"
-#python ExtractGitHubRepos.py $pythonArgs > sampleRepos.txt
-#echo "Finishing ExtractGitHubRepos"
-
-
-#rm -rf GitRepos
-#echo "Creating GitRepos"
-#mkdir GitRepos
 cd GitRepos/
 repoFile=../sampleRepos.txt
 while read projectFolder
 do
-  #echo "git clone $projectFolder"
-  #CLONECOMMAND="git clone $projectFolder"
-  #eval $CLONECOMMAND
-  #echo ""  	
-  #sleep 1
 
-
-#REMOVE THIS LINE
-#cd GitRepos/
-
-#create a file with all the project folders
-#rm -f folders.txt
-#ls > foldersTmp.txt
-#grep -vwE "foldersTmp.txt" foldersTmp.txt > foldersTmp2.txt
-#rm foldersTmp.txt
-#grep -vwE "projectFolders.txt" foldersTmp2.txt > foldersTmp3.txt
-#rm foldersTmp2.txt
-#grep -vwE "logResult.txt" foldersTmp3.txt > projectFolders.txt
-#rm foldersTmp3.txt
-
-#creates commitList.txt with pairs of the hashs of the bug fixing commits
-#projectFolders=projectFolders.txt
-#while read projectFolder
-#do
-
-  folderNameTmp=$(echo $projectFolder | sed 's#.*/##g')
-  folderName=$(echo "${folderNameTmp::-4}") #take the folder name from the url
+  folderNameTmp=$(echo $projectFolder | sed 's#.*/##g') #take the folder name from the url with extension
+  folderName=$(echo "${folderNameTmp::-4}") # remove extension
   echo "Working on project $folderName"
   cd $folderName
-  git log > logResult.txt
+  git log --all -- '*.java' > logResult.txt # look only for commits that look into java files
   python ../../readLogData.py logResult.txt commitList.txt
   echo "commitList.txt created"
 
@@ -120,7 +85,6 @@ do
           git show $afterHash:$fileName > $nameOfFile
         done < $fileNames
         cd .. #get out from after folder
-      
 
         fileNames=./filesModifiedInThisCommit.txt
         while read fileName
@@ -129,7 +93,7 @@ do
           currentDirectory=$(pwd)
           nameOfFileWithoutExtension="${nameOfFile%.*}"
           echo "Calling QACrashFix for project $folderName in commit $commitNumber with file $nameOfFile"
-	  /usr/lib/jvm/java-7-oracle/bin/java -Dfile.encoding=UTF-8 -classpath $QACRASHFIXDIRECTORY/test/Test/bin:$QACRASHFIXDIRECTORY/target/exception-fix-0.0.1-SNAPSHOT.jar:$QACRASHFIXDIRECTORY/test/Test/lib/log4j-api-2.5.jar:$QACRASHFIXDIRECTORY/test/Test/lib/log4j-core-2.5.jar:$QACRASHFIXDIRECTORY/test/Test/lib/org.eclipse.core.contenttype_3.5.0.v20150421-2214.jar:$QACRASHFIXDIRECTORY/test/Test/lib/org.eclipse.core.jobs_3.7.0.v20150330-2103.jar:$QACRASHFIXDIRECTORY/test/Test/lib/org.eclipse.core.resources_3.10.1.v20150725-1910.jar:$QACRASHFIXDIRECTORY/test/Test/lib/org.eclipse.core.runtime_3.11.1.v20150903-1804.jar:$QACRASHFIXDIRECTORY/test/Test/lib/org.eclipse.equinox.common_3.7.0.v20150402-1709.jar:$QACRASHFIXDIRECTORY/test/Test/lib/org.eclipse.equinox.preferences_3.5.300.v20150408-1437.jar:$QACRASHFIXDIRECTORY/test/Test/lib/org.eclipse.jdt.core_3.11.1.v20150902-1521.jar:$QACRASHFIXDIRECTORY/test/Test/lib/org.eclipse.osgi_3.10.102.v20160118-1700.jar Test $currentDirectory/before/$nameOfFile $currentDirectory/after/$nameOfFile > "$currentDirectory"$nameOfFileWithoutExtension.txt
+	  /usr/lib/jvm/java-7-oracle/bin/java -Dfile.encoding=UTF-8 -classpath $QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/bin:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/target/exception-fix-0.0.1-SNAPSHOT.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/log4j-api-2.5.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/log4j-core-2.5.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/org.eclipse.core.contenttype_3.5.0.v20150421-2214.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/org.eclipse.core.jobs_3.7.0.v20150330-2103.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/org.eclipse.core.resources_3.10.1.v20150725-1910.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/org.eclipse.core.runtime_3.11.1.v20150903-1804.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/org.eclipse.equinox.common_3.7.0.v20150402-1709.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/org.eclipse.equinox.preferences_3.5.300.v20150408-1437.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/org.eclipse.jdt.core_3.11.1.v20150902-1521.jar:$QACRASHFIXDIRECTORY/qacrashfix/QACrashFix/test/Test/lib/org.eclipse.osgi_3.10.102.v20160118-1700.jar Test $currentDirectory/before/$nameOfFile $currentDirectory/after/$nameOfFile > "$currentDirectory"$nameOfFileWithoutExtension.txt
 
         done < $fileNames
       fi
