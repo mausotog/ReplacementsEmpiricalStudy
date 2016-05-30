659a743829a7bf05e45f918459d79046fa615d39
mau <mausotog@gmail.com>
2016-05-30 12:10:32 -0400

    creating script to get the accuracy of the model. 3 try

--- 7testAccuracyOfModel.sh
+++ 7testAccuracyOfModel.sh
@@ -94,52 +94,6 @@ do
 
       done
 
-      
-
-
-
-
-
-
-
-
-
-  #for tierNumber in {1..10}
-  #do
-  #  if [ $tierNumber -ne $FOLDTOOMIT ]; then
-  #    cd tier"$tierNumber"
-  #    for i in `seq 1 484`; # 484 = 22*22 all possible combinations of replacements
-  #      do
-  #        commandToGetSpecificLine="sed '$i!d' tier"$tierNumber"Model.txt" # take number in line i
-  #        numberOfReplacementsOfThisLine=$(eval $commandToGetSpecificLine)
-  #        (( replacementCounter[$i]+=$numberOfReplacementsOfThisLine ))
-  #        #if [ ${replacementCounter[$i]} -ne 0 ]; then
-  #          #echo "Total counter for iterator $i: ${replacementCounter[$i]}"
-  #        #fi
-  #    done 
-  #    cd .. # out of tier folder
-  #  fi
-  #done
-
-  #print the replacementCounter array into a file here: Model.txt
-  #printf "%s\n" "${replacementCounter[@]}" > AllButTier"$FOLDTOOMIT"Model.txt
-
-  #touch summaryOfModel.txt
-  #for i in `seq 1 484`; # 484 = 22*22 all possible combinations of replacements
-  #do
-  #  if [ ${replacementCounter[$i]} -ne 0 ]; then
-  #    echo "Total counter for iterator $i: ${replacementCounter[$i]}" >> summaryOfAllButTier"$FOLDTOOMIT"Model$
-  #  fi
-  #done 
-
-
-
-
-
-
-
-
-
     cd .. #out of tier$FOLDTOTEST
   cd .. # out of GitRepos
 done
